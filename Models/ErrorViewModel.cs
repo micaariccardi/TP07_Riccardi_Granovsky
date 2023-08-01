a90fc8c0d4ee8092ec220fed7197df7ae2081f94
MICAELA RICCARDI <47574710@ort>
2023-08-01 12:07:50 -0300

    inicio y diseño de sql

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP07_Riccardi_Granovsky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
